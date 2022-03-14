7742289041c633dc22f465f5a556c805b3f79fbe
juanrol <juanfranrol@icloud.com>
2022-03-14 18:29:38 -0300

    content rendered without style

--- build/bundle.js
+++ build/bundle.js
@@ -2880,7 +2880,7 @@ module.exports.formatError = function (err) {
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("ccb3529f04256b81af9e")
+/******/ 		__webpack_require__.h = () => ("bb8594fc073894f439a8")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
@@ -3215,11 +3215,41 @@ var cityList; //number of cities
 
 var cityCount;
 var cityWeatherCount;
-var SOURCE = "http://localhost:3000/cities.xml"; //PRIVATE METHODS
+var SOURCE = "http://localhost:3000/cities.xml";
+var temperature = 0.00;
+var precipitationMode; //PRIVATE METHODS
+//function to convert K to C
+
+function convertToC(myK) {
+  temperature = myK - 273.15;
+  return temperature.toFixed(2);
+}
 
 function populateContent() {
-  var txtClouds = cityWeatherXML.querySelectorAll("clouds")[0].getAttribute("name");
-  console.log(txtClouds);
+  document.querySelector(".weather-desc__description").innerHTML = cityWeatherXML.querySelectorAll("clouds")[0].getAttribute("name");
+  document.querySelector(".weather-desc__city").innerHTML = cityList.selectedOptions[0].text;
+  temperature = parseFloat(cityWeatherXML.querySelectorAll("temperature")[0].getAttribute("value"));
+  document.querySelector(".main-content__temperature__current").innerHTML = convertToC(temperature) + " °C Current";
+  temperature = parseFloat(cityWeatherXML.querySelectorAll("temperature")[0].getAttribute("min"));
+  document.querySelector(".main-content__temperature__low").innerHTML = convertToC(temperature) + " °C Low";
+  temperature = parseFloat(cityWeatherXML.querySelectorAll("temperature")[0].getAttribute("max"));
+  document.querySelector(".main-content__temperature__high").innerHTML = convertToC(temperature) + " °C High";
+  temperature = parseFloat(cityWeatherXML.querySelectorAll("feels_like")[0].getAttribute("value"));
+  document.querySelector(".main-content__temperature__feels").innerHTML = "Feels like " + convertToC(temperature) + " °C";
+  precipitationMode = cityWeatherXML.querySelectorAll("precipitation")[0].getAttribute("mode");
+
+  if (precipitationMode == "no") {
+    document.querySelector(".main-content__precipitation__value").innerHTML = "0 mm";
+  } else {
+    document.querySelector(".main-content__precipitation__value").innerHTML = cityWeatherXML.querySelectorAll("precipitation")[0].getAttribute("value") + "mm";
+  }
+
+  document.querySelector(".main-content__humidity__value").innerHTML = cityWeatherXML.querySelectorAll("humidity")[0].getAttribute("value") + " %";
+  document.querySelector(".main-content__pressure__value").innerHTML = cityWeatherXML.querySelectorAll("pressure")[0].getAttribute("value") + " hPa";
+  document.querySelector(".main-content__wind__direction").innerHTML = cityWeatherXML.querySelectorAll("direction")[0].getAttribute("name") + " Wind";
+  document.querySelector(".main-content__wind__strength").innerHTML = cityWeatherXML.querySelectorAll("speed")[0].getAttribute("name");
+  var speedKMH = cityWeatherXML.querySelectorAll("speed")[0].getAttribute("value") * (36 / 10);
+  document.querySelector(".main-content__wind__speed").innerHTML = speedKMH.toFixed(2) + " km/h speed";
 }
 
 function populateList() {
@@ -3238,6 +3268,7 @@ function populateList() {
 function onApiLoaded(result) {
   cityWeatherXML = result;
   cityWeatherCount = cityWeatherXML.querySelectorAll("current").length;
+  console.log(cityWeatherXML);
 
   if (cityWeatherCount == 1) {
     populateContent();
