8cae515a1523990ba96149fea424a1f196af3988
juanrol <juanfranrol@icloud.com>
2022-03-15 11:56:14 -0300

    Applied CSS, weather icons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // importing the sass stylesheet for bundling
 import "./../sass/styles.scss";
+import "./../sass/weather-icons.scss";
+
 import { getXMLData } from "./Toolkit";
 
 let citiesXML;
@@ -30,9 +32,10 @@ function convertToC(myK)
 }
 
 
+//function that populates content according to chosen city
 function populateContent()
 {
-    
+    //output data
     document.querySelector(".weather-desc__description").innerHTML = cityWeatherXML.querySelectorAll("clouds")[0].getAttribute("name");
     document.querySelector(".weather-desc__city").innerHTML = cityList.selectedOptions[0].text;
     temperature = parseFloat(cityWeatherXML.querySelectorAll("temperature")[0].getAttribute("value"));
@@ -44,7 +47,10 @@ function populateContent()
     temperature = parseFloat(cityWeatherXML.querySelectorAll("feels_like")[0].getAttribute("value"));
     document.querySelector(".main-content__temperature__feels").innerHTML = "Feels like " + convertToC(temperature) + " °C";
     
+    //store precipitation mode
     precipitationMode = cityWeatherXML.querySelectorAll("precipitation")[0].getAttribute("mode");
+    
+    //show 0 if precipitation mode is 0 else show mm of precipitation
     if (precipitationMode =="no")
     {
         document.querySelector(".main-content__precipitation__value").innerHTML = "0 mm";    
@@ -55,23 +61,18 @@ function populateContent()
     }
 
     document.querySelector(".main-content__humidity__value").innerHTML = cityWeatherXML.querySelectorAll("humidity")[0].getAttribute("value") + " %";
+    
     document.querySelector(".main-content__pressure__value").innerHTML = cityWeatherXML.querySelectorAll("pressure")[0].getAttribute("value") + " hPa";
-    document.querySelector(".main-content__wind__direction").innerHTML = cityWeatherXML.querySelectorAll("direction")[0].getAttribute("name") + " Wind";
-    
-    
-    
+    document.querySelector(".main-content__wind__direction").innerHTML = cityWeatherXML.querySelectorAll("direction")[0].getAttribute("name") + " wind";
     document.querySelector(".main-content__wind__strength").innerHTML = cityWeatherXML.querySelectorAll("speed")[0].getAttribute("name");
-    
     let speedKMH = cityWeatherXML.querySelectorAll("speed")[0].getAttribute("value") * (36/10);
     document.querySelector(".main-content__wind__speed").innerHTML = speedKMH.toFixed(2) + " km/h speed";
-
-    
-
-
 }
 
+//Function to populate select
 function populateList()
 {
+    //add options to select and set properties
     for (let i = 0; i < cityCount; i++)
     {
         let option = document.createElement("option");
@@ -81,8 +82,6 @@ function populateList()
     }
     cityList.addEventListener("change",onListItemChanged);
 }
-
-
 
 
 // ----------------------------------EVENT HANDLERS
@@ -99,9 +98,15 @@ function onApiLoaded(result)
 
 }
 
-//list event handler
+//select event handler
 function onListItemChanged(e)
 {
+    
+    //hide error and show content
+    document.querySelector(".error").style.display="none";
+    document.querySelector(".main-content").style.display="flex";
+    document.querySelector(".weather-desc").style.display="block";
+    
     let option = cityList.selectedOptions[0];
     let apiURL = "http://api.openweathermap.org/data/2.5/weather?q=" + option.name + ",CA&mode=xml&appid=6983b0f0351df4922a129a07e4b832b9";
     getXMLData(apiURL,onApiLoaded,onError);
@@ -111,6 +116,8 @@ function onListItemChanged(e)
 function onLoaded(result)
 {
     citiesXML = result;
+
+    console.log("test" + citiesXML.querySelectorAll("Alberta")[1]);
      //number of cities
     cityCount = citiesXML.querySelectorAll("city").length;
     if (cityCount > 0)
@@ -121,20 +128,24 @@ function onLoaded(result)
     
 }
 
-//if an error occurs in getting the xml data
+//if an error occurs in getting the xml data or api
 function onError(e)
 {
+    //show error
+    document.querySelector(".main-content").style.display="none";
+    document.querySelector(".weather-desc").style.display="none";
+    document.querySelector(".error").style.display="block";
     console.log("Error: Ajax request problem");
 }
 
 // ----------------------------------------------- main method
 function main() 
 {
+    //stablish select element
     cityList = document.querySelector(".selector__city");
 
     //construct the XMLHttpRequest object
     getXMLData(SOURCE,onLoaded,onError);
-
 }
 
 main();
